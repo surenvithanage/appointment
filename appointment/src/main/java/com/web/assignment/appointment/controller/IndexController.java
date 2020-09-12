ac02612c24e719baa55ccde727dc872c625753ac
suren <surenanthonyvithanage@gmail.com>
2020-09-12 21:26:16 +0530

    Updated

---
+++ appointment/src/main/java/com/web/assignment/appointment/controller/IndexController.java
@@ -0,0 +1,43 @@
+package com.web.assignment.appointment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("treatments")
+    public String treatments() {
+        return "treatments";
+    }
+
+    @GetMapping("login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("profile")
+    public String profile() {
+        return "profile";
+    }
+}
